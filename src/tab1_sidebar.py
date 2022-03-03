726f4f73678eeb2a0386cd91dd45585bedf21f65
Macy Chan <macychan@student.ubc.ca>
2022-03-02 20:24:56 -0800

    Remove unused dropdown

--- src/tab1_sidebar.py
+++ src/tab1_sidebar.py
@@ -6,86 +6,71 @@ import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
 
-df_all = pd.read_csv("data/Primary-energy-consumption-from-fossilfuels-nuclear-renewables.csv")
+df_all = pd.read_csv(
+    "data/Primary-energy-consumption-from-fossilfuels-nuclear-renewables.csv"
+)
 
-df_notna = df_all[df_all['Code'].notna()]
-df_countries = df_notna[df_notna['Code']!='OWID_WRL']
-df_world = df_notna[df_notna['Code']=='OWID_WRL']
-df_continents = df_all[df_all['Code'].isna()]
+df_notna = df_all[df_all["Code"].notna()]
+df_countries = df_notna[df_notna["Code"] != "OWID_WRL"]
+df_world = df_notna[df_notna["Code"] == "OWID_WRL"]
+df_continents = df_all[df_all["Code"].isna()]
 
-list_of_continents = df_continents['Entity'].unique()
-list_of_countries = df_countries['Entity'].unique()
+list_of_continents = df_continents["Entity"].unique()
+list_of_countries = df_countries["Entity"].unique()
 
-#==============================================================================
+# ==============================================================================
 #                            SideBar1
-#==============================================================================  
+# ==============================================================================
 
 SIDEBAR1_STYLE = {
     "position": "fixed",
     "top": 0,
     "left": 0,
     "bottom": 0,
-    #"width": "18rem",
+    # "width": "18rem",
     "padding": "2rem 1rem",
     "background-image": "url(/assets/wind-energy.jpg)",
     # "background-color": "#98FB98",
     "background-color": "rgba(255,255,255,0.6)",
     "background-blend-mode": "overlay",
-
 }
 
 
-sidebar1 = dbc.Col([
+sidebar1 = dbc.Col(
+    [
         html.H3("World Energy Visualisation"),
         html.Br(),
         html.H5(
             "Energy type",
-            #style={"width": "50%", "display": "inline-block"},
+            # style={"width": "50%", "display": "inline-block"},
         ),
-        
         dbc.Row(
             dcc.Dropdown(
                 id="tab1-energy-type-dropdown",
-                options=[{"label": energy_type, "value": energy_type} for energy_type in ["Fossil", "Nuclear", "Renewables"]],
+                options=[
+                    {"label": energy_type, "value": energy_type}
+                    for energy_type in ["Fossil", "Nuclear", "Renewables"]
+                ],
                 value="Fossil",
             ),
-            #width=12,
+            # width=12,
             style={
                 "padding": "10px 10px 10px 0px",
             },
         ),
         html.Br(),
-        
-        html.H5(
-            "Region",
-            style={"width": "50%", "display": "inline-block"},
-        ),
-        
-        dbc.Row(
-            dcc.Dropdown(
-                id="tab1-region-dropdown",
-                options=[{"label": region, "value": region} for region in list_of_continents],
-                multi=True,
-                value=["North America", "Europe"],
-            ),
-            #width=12,
-            style={
-                "padding": "10px 10px 10px 0px",
-            },
-        ),
-        html.Br(),
-        
         html.H5(
             "Data sources",
             style={"width": "50%", "display": "inline-block"},
         ),
         dbc.Row(
-            dcc.Markdown('''
+            dcc.Markdown(
+                """
                 Datasets for visualization of energy trends were downloaded from [here](https://www.kaggle.com/donjoeml/energy-consumption-and-generation-in-the-globe)
-            '''),
+            """
+            ),
         ),
     ],
     md=2,
     style=SIDEBAR1_STYLE,
 )
-
--- src/tab2_trends.py
+++ src/tab2_trends.py
@@ -8,139 +8,138 @@ import plotly.io as pio
 import plotly.graph_objects as go
 
 
-df_all = pd.read_csv("data/Primary-energy-consumption-from-fossilfuels-nuclear-renewables.csv")
+df_all = pd.read_csv(
+    "data/Primary-energy-consumption-from-fossilfuels-nuclear-renewables.csv"
+)
 
-df_notna = df_all[df_all['Code'].notna()]
-df_countries = df_notna[df_notna['Code']!='OWID_WRL']
-df_world = df_notna[df_notna['Code']=='OWID_WRL']
-df_continents = df_all[df_all['Code'].isna()]
+df_notna = df_all[df_all["Code"].notna()]
+df_countries = df_notna[df_notna["Code"] != "OWID_WRL"]
+df_world = df_notna[df_notna["Code"] == "OWID_WRL"]
+df_continents = df_all[df_all["Code"].isna()]
 
-list_of_continents = df_continents['Entity'].unique()
-list_of_countries = df_countries['Entity'].unique()
+list_of_continents = df_continents["Entity"].unique()
+list_of_countries = df_countries["Entity"].unique()
 
-#==============================================================================
+# ==============================================================================
 #                 Placeholder for valid plots, remove afterwards
-#==============================================================================  
-list_yrs = df_all['Year'].unique()
+# ==============================================================================
+list_yrs = df_all["Year"].unique()
 
-#==============================================================================
+# ==============================================================================
 #                            Layout for lineplots
-#==============================================================================   
+# ==============================================================================
 
-tab2_lineplots = dbc.Col([
-        html.Div([
-            html.P(
-                "Select the year range for the below plots:",
-                style={
-                    "color": "#888888"
-                }
-            ),
-            dcc.RangeSlider(
-                min=list_yrs.min(), 
-                max=list_yrs.max(), 
-                step=1, 
-                value=[list_yrs.min(), list_yrs.max()],
-                marks={int(i): str(i) for i in np.append(list_yrs[::5], [list_yrs.max()])},
-                tooltip={"placement": "top", "always_visible": False},
-                id="tab2-years-rangeslider")],
-            style={
-                "padding-top": "30px"
-            }
+tab2_lineplots = dbc.Col(
+    [
+        html.Div(
+            [
+                html.P(
+                    "Select the year range for the below plots:",
+                    style={"color": "#888888"},
+                ),
+                dcc.RangeSlider(
+                    min=list_yrs.min(),
+                    max=list_yrs.max(),
+                    step=1,
+                    value=[list_yrs.min(), list_yrs.max()],
+                    marks={
+                        int(i): str(i)
+                        for i in np.append(list_yrs[::5], [list_yrs.max()])
+                    },
+                    tooltip={"placement": "top", "always_visible": False},
+                    id="tab2-years-rangeslider",
+                ),
+            ],
+            style={"padding-top": "30px"},
         ),
         html.Br(),
-
-        #html.H4("Fossil"),
         dcc.Graph(id="tab2-lineplot-fossil"),
-        #html.Br(),
-        
-        #html.H4("Nuclear"),
         dcc.Graph(id="tab2-lineplot-nuclear"),
-        #html.Br(),
-        
-        #html.H4("Renewable"),
-        dcc.Graph(id="tab2-lineplot-renewable")
-        
-    ])
+        dcc.Graph(id="tab2-lineplot-renewable"),
+    ]
+)
 
 
-#==============================================================================
+# ==============================================================================
 #                            Lineplots
-#==============================================================================
+# ==============================================================================
+
 
 @callback(
-    Output("tab2-lineplot-fossil", "figure"), 
-    Input("tab2-country-dropdown", "value"), 
-    Input("tab2-region-dropdown", "value"), 
-    Input("tab2-world-toggle", "value"), 
-    Input("tab2-years-rangeslider", "value"))
+    Output("tab2-lineplot-fossil", "figure"),
+    Input("tab2-country-dropdown", "value"),
+    Input("tab2-region-dropdown", "value"),
+    Input("tab2-world-toggle", "value"),
+    Input("tab2-years-rangeslider", "value"),
+)
 def lineplot_fossil(country, region, toggle, years):
     """
     Docs
     """
-    df_use = df_all[df_all['Entity'].isin([country] + region + len(toggle) * ["World"])]
-    df_use = df_use[(df_use['Year'] >= years[0]) & (df_use["Year"] <= years[1])]
-    fig = px.line(df_use, x="Year", y="Fossil fuels (% sub energy)", color="Entity", 
-        title=f'Fossil fuels usage from {years[0]} to {years[1]}')
+    df_use = df_all[df_all["Entity"].isin([country] + region + len(toggle) * ["World"])]
+    df_use = df_use[(df_use["Year"] >= years[0]) & (df_use["Year"] <= years[1])]
+    fig = px.line(
+        df_use,
+        x="Year",
+        y="Fossil fuels (% sub energy)",
+        color="Entity",
+        title=f"Fossil fuels usage from {years[0]} to {years[1]}",
+    )
     fig.update_layout(
-        title={
-            "x": 0.5,
-            "y": 0.8,
-            "xanchor": "center",
-            "yanchor": "bottom"
-        }
+        title={"x": 0.5, "y": 0.8, "xanchor": "center", "yanchor": "bottom"}
     )
     return fig
-    
-    
+
+
 @callback(
-    Output("tab2-lineplot-nuclear", "figure"), 
-    Input("tab2-country-dropdown", "value"), 
-    Input("tab2-region-dropdown", "value"), 
-    Input("tab2-world-toggle", "value"), 
-    Input("tab2-years-rangeslider", "value"))
+    Output("tab2-lineplot-nuclear", "figure"),
+    Input("tab2-country-dropdown", "value"),
+    Input("tab2-region-dropdown", "value"),
+    Input("tab2-world-toggle", "value"),
+    Input("tab2-years-rangeslider", "value"),
+)
 def lineplot_nuclear(country, region, toggle, years):
     """
     Docs
     """
-    df_use = df_all[df_all['Entity'].isin([country] + region + len(toggle) * ["World"])]
-    df_use = df_use[(df_use['Year'] >= years[0]) & (df_use["Year"] <= years[1])]
-    fig = px.line(df_use, x="Year", y="Nuclear (% sub energy)", color="Entity", 
-        title=f'Nuclear fuel usage from {years[0]} to {years[1]}')
+    df_use = df_all[df_all["Entity"].isin([country] + region + len(toggle) * ["World"])]
+    df_use = df_use[(df_use["Year"] >= years[0]) & (df_use["Year"] <= years[1])]
+    fig = px.line(
+        df_use,
+        x="Year",
+        y="Nuclear (% sub energy)",
+        color="Entity",
+        title=f"Nuclear fuel usage from {years[0]} to {years[1]}",
+    )
     fig.update_layout(
-        title={
-            "x": 0.5,
-            "y": 0.8,
-            "xanchor": "center",
-            "yanchor": "bottom"
-        }
+        title={"x": 0.5, "y": 0.8, "xanchor": "center", "yanchor": "bottom"}
     )
 
     return fig
-    
-    
+
+
 @callback(
-    Output("tab2-lineplot-renewable", "figure"), 
-    Input("tab2-country-dropdown", "value"), 
-    Input("tab2-region-dropdown", "value"), 
-    Input("tab2-world-toggle", "value"), 
-    Input("tab2-years-rangeslider", "value"))
+    Output("tab2-lineplot-renewable", "figure"),
+    Input("tab2-country-dropdown", "value"),
+    Input("tab2-region-dropdown", "value"),
+    Input("tab2-world-toggle", "value"),
+    Input("tab2-years-rangeslider", "value"),
+)
 def lineplot_renewable(country, region, toggle, years):
     """
     Docs
     """
-    df_use = df_all[df_all['Entity'].isin([country] + region + len(toggle) * ["World"])]
-    df_use = df_use[(df_use['Year'] >= years[0]) & (df_use["Year"] <= years[1])]
-    fig = px.line(df_use, x="Year", y="Renewables (% sub energy)", color="Entity", 
-        title=f'Renewables usage from {years[0]} to {years[1]}')
-    fig.update_layout(
-        title={
-            "x": 0.5,
-            "y": 0.8,
-            "xanchor": "center",
-            "yanchor": "bottom"
-        }
+    df_use = df_all[df_all["Entity"].isin([country] + region + len(toggle) * ["World"])]
+    df_use = df_use[(df_use["Year"] >= years[0]) & (df_use["Year"] <= years[1])]
+    fig = px.line(
+        df_use,
+        x="Year",
+        y="Renewables (% sub energy)",
+        color="Entity",
+        title=f"Renewables usage from {years[0]} to {years[1]}",
     )
-    
+    fig.update_layout(
+        title={"x": 0.5, "y": 0.8, "xanchor": "center", "yanchor": "bottom"}
+    )
+
     return fig
-    
-    
