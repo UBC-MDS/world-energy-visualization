61f3847676cf7ec258731abdf66ad7fb20cde155
Pavel Levchenko <plevchen@student.ubc.ca>
2022-03-18 13:53:54 -0800

    updated dbcCards for trend plots

--- src/tab2_trends.py
+++ src/tab2_trends.py
@@ -37,7 +37,7 @@ tab2_lineplots = dbc.Col(
                     min=list_yrs.min(),
                     max=list_yrs.max(),
                     step=1,
-                    value=[list_yrs.min(), list_yrs.max()],
+                    value=[1975, 2010],
                     marks={
                         int(i): str(i)
                         for i in np.append(list_yrs[::5], [list_yrs.max()])
@@ -49,9 +49,37 @@ tab2_lineplots = dbc.Col(
             style={"padding-top": "30px"},
         ),
         html.Br(),
-        dcc.Graph(id="tab2-lineplot-fossil"),
-        dcc.Graph(id="tab2-lineplot-nuclear"),
-        dcc.Graph(id="tab2-lineplot-renewable"),
+        dbc.Card([
+            dbc.CardHeader('Fossil fuel usage', 
+                style={'fontWeight': 'bold', 'color':'white','font-size': '22px', 'backgroundColor':'#3f5cfe','width': '100%', 'height': '50px'}),
+            dbc.CardBody([
+                    dcc.Graph(id="tab2-lineplot-fossil")
+                ], 
+                    style={'border-width': '0', 'width': '100%',}
+                )
+        ]),
+        html.Br(),
+        
+        dbc.Card([
+            dbc.CardHeader('Nuclear power usage', 
+                style={'fontWeight': 'bold', 'color':'white','font-size': '22px', 'backgroundColor':'#43aeff','width': '100%', 'height': '50px'}),
+            dbc.CardBody([
+                    dcc.Graph(id="tab2-lineplot-nuclear")
+                ], 
+                    style={'border-width': '0', 'width': '100%',}
+                )
+        ]),
+        html.Br(),
+        
+        dbc.Card([
+            dbc.CardHeader('Renewable energy usage', 
+                style={'fontWeight': 'bold', 'color':'white','font-size': '22px', 'backgroundColor':'#3dbc77','width': '100%', 'height': '50px'}),
+            dbc.CardBody([
+                    dcc.Graph(id="tab2-lineplot-renewable")
+                ], 
+                    style={'border-width': '0', 'width': '100%',}
+                )
+        ]),
     ]
 )
 
@@ -79,11 +107,12 @@ def lineplot_fossil(country, region, toggle, years):
         x="Year",
         y="Fossil",
         color="Entity",
-        title=f"Fossil fuels usage from {years[0]} to {years[1]}",
     )
     fig.update_layout(
         title={"x": 0.5, "y": 0.8, "xanchor": "center", "yanchor": "bottom"},
-        yaxis_title="Fossil fuel Usage (%)"
+        yaxis_title="Fossil fuel Usage (%)", 
+        xaxis_title="", 
+        legend_title_text=''
     )
     return fig
 
@@ -106,11 +135,12 @@ def lineplot_nuclear(country, region, toggle, years):
         x="Year",
         y="Nuclear",
         color="Entity",
-        title=f"Nuclear fuel usage from {years[0]} to {years[1]}",
     )
     fig.update_layout(
         title={"x": 0.5, "y": 0.8, "xanchor": "center", "yanchor": "bottom"},
-        yaxis_title="Nuclear energy Usage (%)"
+        yaxis_title="Nuclear energy Usage (%)", 
+        xaxis_title="",
+        legend_title_text=''
     )
 
     return fig
@@ -134,11 +164,12 @@ def lineplot_renewable(country, region, toggle, years):
         x="Year",
         y="Renewables",
         color="Entity",
-        title=f"Renewables usage from {years[0]} to {years[1]}",
     )
     fig.update_layout(
         title={"x": 0.5, "y": 0.8, "xanchor": "center", "yanchor": "bottom"},
-        yaxis_title="Renewable energy Usage (%)"
+        yaxis_title="Renewable energy Usage (%)", 
+        xaxis_title="",
+        legend_title_text=''
     )
 
     return fig
